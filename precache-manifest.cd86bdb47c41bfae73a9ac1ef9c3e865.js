879679dab2643ee8ca79f64617ee382fbef6a312
Donald Tillman <till@Jacks-iMac-2.local>
2019-05-28 15:43:17 -0700

    Updates

---
+++ precache-manifest.cd86bdb47c41bfae73a9ac1ef9c3e865.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8dbac2b2529b84dcbb332e9825fea67a",
+    "url": "/lincoln/index.html"
+  },
+  {
+    "revision": "5f264838e53ba6117262",
+    "url": "/lincoln/static/css/main.955ac98e.chunk.css"
+  },
+  {
+    "revision": "9a5c0cc5b7b5942488d9",
+    "url": "/lincoln/static/js/2.56cb80cd.chunk.js"
+  },
+  {
+    "revision": "5f264838e53ba6117262",
+    "url": "/lincoln/static/js/main.0fab8061.chunk.js"
+  },
+  {
+    "revision": "b2a459f6be13f35e34c3",
+    "url": "/lincoln/static/js/runtime~main.bb0acb25.js"
+  }
+]);
